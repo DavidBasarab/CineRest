723ad2b63223a2055a401568b4f8ee81f986e430
David Basarab <dbasarab617@gmail.com>
2014-02-27 16:25:41 -0500

    Tring to get touch working for dragging

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var currentImageId = 0;
 app = {
     initialize: function () {
         var self = this;
+        startTouchHandler();
         $('.dragMe').draggable({
             revert: true,
             stack: '.dragMe',
@@ -44,6 +45,38 @@ function handlePanelDrop(event, ui) {
     var sourceId = sourceDiv.attr('id').replace('source', '') - 1;
 
 
-    $('<div style="height: 184px; width: 326px;"><img src="http://192.168.1.58/CineRest/Source/Image/' + sourceId + '" style="width: 100%; height: 100%;"/></div>')
+    $('<div class="populatedPanel"><img src="http://192.168.1.58/CineRest/Source/Image/' + sourceId + '" style="width: 100%; height: 100%;"/></div>')
         .appendTo(panel);
+}
+
+function startTouchHandler() {
+    document.addEventListener("touchstart", touchHandler, true);
+    document.addEventListener("touchmove", touchHandler, true);
+    document.addEventListener("touchend", touchHandler, true);
+    document.addEventListener("touchcancel", touchHandler, true);
+}
+function touchHandler(event) {
+    var touches = event.changedTouches,
+        first = touches[0],
+        type = "";
+    switch (event.type) {
+        case "touchstart":
+            type = "mousedown";
+            break;
+        case "touchmove":
+            type = "mousemove";
+            break;
+        case "touchend":
+            type = "mouseup";
+            break;
+        default:
+            return;
+    }
+    var simulatedEvent = document.createEvent("MouseEvent");
+    simulatedEvent.initMouseEvent(type, true, true, window, 1,
+        first.screenX, first.screenY,
+        first.clientX, first.clientY, false,
+        false, false, false, 0/*left*/, null);
+    first.target.dispatchEvent(simulatedEvent);
+    event.preventDefault();
 }
