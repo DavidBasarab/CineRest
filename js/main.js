998dfa5c0dc0b6324472ae25d560a386ca73b925
David Basarab <dbasarab617@gmail.com>
2014-02-27 11:57:45 -0500

    Add config stuff

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ app = {
 app.initialize();
 
 function changeSourceImage() {
-    $('#testImage').attr('src', 'http://localhost/CineRest/Source/Image/' + currentImageId + '?' + new Date().getTime());
+    $('#testImage').attr('src', 'http://192.168.1.58/CineRest/Source/Image/' + currentImageId + '?' + new Date().getTime());
     currentImageId++;
     if(currentImageId > 5) currentImageId = 0;
     window.setTimeout(function () {
