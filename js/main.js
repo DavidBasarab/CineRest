d34d996b9a9974127a2c5ac8ae11bc0d3b36fcc5
David Basarab <dbasarab617@gmail.com>
2015-03-23 11:05:41 -0400

    Finally got requests working

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
 
 app = {
     initialize: function () {
-
-        //startTouchHandler();
-
-        sourceView.initialize();
-
-        //panelView.initialize();
+        getWorkspaces();
     }
 }
 
 app.initialize();
+
+
+function getServer() {
+    return 'http://192.168.1.125/DBasarabCinRestService';
+}
+
+function getWorkspaces() {
+    $.ajax({
+        url: getServer() + '/Workspace',
+        type: 'GET',
+        success: function(data) {
+            $('.main_content').html(JSON.stringify(data));
+            //$('.workspaceSelect').change();
+        },
+        error: function(xhr) {
+            alert('xhr.Status = ' + xhr.status);
+        }
+    });
+}
